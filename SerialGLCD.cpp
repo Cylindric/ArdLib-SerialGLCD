2e4c48163c7759de3abed4adfb03ac4fc971c54d
Mark Hanford <mark@hanfordonline.co.uk>
2012-01-17 15:28:24 +0000

    Created initial API for the SerialGLCD

--- SerialGLCD.cpp
+++ SerialGLCD.cpp
@@ -2,25 +2,152 @@
 #include "SerialGLCD.h"
 #include <SoftwareSerial.h>
 
-SerialGLCD::SerialGLCD() {
-  this->_serial = SoftwareSerial(1,2);
+// Command-codes for the SparkFun SerialGLCD Backpack
+// http://www.sparkfun.com/products/9352
+static const uint8_t GLCDCMD_BAUD = 0x07;
+static const uint8_t GLCDCMD_BOX = 0x0F;
+static const uint8_t GLCDCMD_CIRCLE = 0x03;
+static const uint8_t GLCDCMD_CLEAR = 0x00;
+static const uint8_t GLCDCMD_COMMAND = 0x7C;
+static const uint8_t GLCDCMD_DEMO = 0x04;
+static const uint8_t GLCDCMD_DUTYCYCLE = 0x02;
+static const uint8_t GLCDCMD_ERASE = 0x05;
+static const uint8_t GLCDCMD_LINE = 0x0C;
+static const uint8_t GLCDCMD_PIXEL = 0x10;
+static const uint8_t GLCDCMD_PX_OFF = 0x00;
+static const uint8_t GLCDCMD_PX_ON = 0x01;
+static const uint8_t GLCDCMD_REVERSE = 0x12;
+static const uint8_t GLCDCMD_SETX = 0x18;
+static const uint8_t GLCDCMD_SETY = 0x19;
+static const uint8_t GLCDCMD_SPLASH = 0x13;
+
+// Small-font
+static const uint8_t SMALLFONT_0[] = {0,1, 0,2, 0,3, 0,4, 1,0, 1,5, 2,0, 2,5, 3,1, 3,2, 3,3, 3,4};
+static const uint8_t SMALLFONT_1[] = {0,0, 0,4, 1,0, 0,2, 0,3, 0,4, 0,5, 1,0};
+
+
+SerialGLCD::SerialGLCD(uint8_t transmitPin, uint8_t width, uint8_t height) : _serial(SoftwareSerial(0, transmitPin)) {
+  this->_txPin = transmitPin;
+  this->_width = width;
+  this->_height = height;
 }
 
-void SerialGLCD::attach(uint8_t transmitPin) {
-  this->_txPin = transmitPin;
+void SerialGLCD::begin() {
+  this->_serial.begin(115200);
+  this->clear();
 }
 
 void SerialGLCD::clear() {
+  this->_serial.write(GLCDCMD_COMMAND);
+  this->_serial.write(GLCDCMD_CLEAR); 
 }
 
 void SerialGLCD::demo() {
+  this->_serial.write(GLCDCMD_COMMAND);
+  this->_serial.write(GLCDCMD_DEMO);
 }
 
-void SerialGLCD::setX(uint8_t x) {
+void SerialGLCD::toggleReverse() {
+  this->_serial.write(GLCDCMD_COMMAND);
+  this->_serial.write(GLCDCMD_REVERSE);
 }
 
-void SerialGLCD::setY(uint8_t y) {
+void SerialGLCD::toggleSplashScreen() {
+  this->_serial.write(GLCDCMD_COMMAND);
+  this->_serial.write(GLCDCMD_SPLASH);
 }
 
-void SerialGLCD::setXY(uint8_t x, uint8_t y) {
+void SerialGLCD::setDutyCycle(uint8_t percent) {
+  this->_serial.write(GLCDCMD_COMMAND);
+  this->_serial.write(GLCDCMD_DUTYCYCLE);
+  this->_serial.write(percent);
+}
+
+void SerialGLCD::setBaudRate(uint8_t ratecode) {
+  this->_serial.write(GLCDCMD_COMMAND);
+  this->_serial.write(GLCDCMD_BAUD);
+  this->_serial.write(0x30 + ratecode);
+}
+
+void SerialGLCD::gotoX(uint8_t x) {
+  this->_serial.write(GLCDCMD_COMMAND);
+  this->_serial.write(GLCDCMD_SETX);
+  this->_serial.write(x);
+}
+
+void SerialGLCD::gotoY(uint8_t y) {
+  this->_serial.write(GLCDCMD_COMMAND);
+  this->_serial.write(GLCDCMD_SETY);
+  this->_serial.write(y);
+}
+
+void SerialGLCD::gotoXY(uint8_t x, uint8_t y) {
+  this->gotoX(x);
+  this->gotoY(y);
+}
+
+void SerialGLCD::drawAscii(char* text) {
+  this->_serial.write(text);
+}
+
+void SerialGLCD::drawPixel(uint8_t x, uint8_t y, bool state) {
+  this->_serial.write(GLCDCMD_COMMAND);
+  this->_serial.write(GLCDCMD_PIXEL);
+  this->_serial.write(x);
+  this->_serial.write(y);
+  if (state) {
+    this->_serial.write(GLCDCMD_PX_ON);
+  } else {
+    this->_serial.write(GLCDCMD_PX_OFF);
+  }
+}
+
+void SerialGLCD::drawLine(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, bool state) {
+  this->_serial.write(GLCDCMD_COMMAND);
+  this->_serial.write(GLCDCMD_LINE);
+  this->_serial.write(x1);
+  this->_serial.write(y1);
+  this->_serial.write(x2);
+  this->_serial.write(y2);
+  if (state) {
+    this->_serial.write(GLCDCMD_PX_ON);
+  } else {
+    this->_serial.write(GLCDCMD_PX_OFF);
+  }
+}
+
+void SerialGLCD::drawBox(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, bool state) {
+  this->_serial.write(GLCDCMD_COMMAND);
+  this->_serial.write(GLCDCMD_BOX);
+  this->_serial.write(x1);
+  this->_serial.write(x1);
+  this->_serial.write(y2);
+  this->_serial.write(y2);
+  if (state) {
+    this->_serial.write(GLCDCMD_PX_ON);
+  } else {
+    this->_serial.write(GLCDCMD_PX_OFF);
+  }
+}
+
+void SerialGLCD::drawCircle(uint8_t x, uint8_t y, uint8_t radius, bool state) {
+  this->_serial.write(GLCDCMD_COMMAND);
+  this->_serial.write(GLCDCMD_CIRCLE);
+  this->_serial.write(x);
+  this->_serial.write(y);
+  this->_serial.write(radius);
+  if (state) {
+    this->_serial.write(GLCDCMD_PX_ON);
+  } else {
+    this->_serial.write(GLCDCMD_PX_OFF);
+  }
+}
+
+void SerialGLCD::eraseBlock(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2) {
+  this->_serial.write(GLCDCMD_COMMAND);
+  this->_serial.write(GLCDCMD_ERASE);
+  this->_serial.write(x1);
+  this->_serial.write(x1);
+  this->_serial.write(y2);
+  this->_serial.write(y2);
 }
--- SerialGLCD.h
+++ SerialGLCD.h
@@ -8,16 +8,30 @@ class SerialGLCD
 {
   private:
     uint8_t _txPin;
+    uint8_t _width;
+    uint8_t _height;
     SoftwareSerial _serial;
 
   public:
-    SerialGLCD();
-    void attach(uint8_t transmitPin);
+    SerialGLCD(uint8_t transmitPin, uint8_t width, uint8_t height);
+    void begin();
     void clear();
     void demo();
-    void setX(uint8_t x);
-    void setY(uint8_t y);
-    void setXY(uint8_t x, uint8_t y);
+    void gotoX(uint8_t x);
+    void gotoY(uint8_t y);
+    void gotoXY(uint8_t x, uint8_t y);
+    void toggleReverse();
+    void toggleSplashScreen();
+    void setDutyCycle(uint8_t percent);
+    void setBaudRate(uint8_t ratecode);
+    
+    void drawAscii(char* text);
+    void drawPixel(uint8_t x, uint8_t y, bool state = true);
+    void drawLine(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, bool state = true);
+    void drawBox(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, bool state = true);
+    void drawCircle(uint8_t x, uint8_t y, uint8_t radius, bool state = true);
+    void eraseBlock(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2);
+    
 };
 
 #endif
