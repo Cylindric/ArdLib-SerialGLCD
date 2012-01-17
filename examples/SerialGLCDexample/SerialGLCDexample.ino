a240fc4efae6b80eabf5569c27ce8e4308979954
Mark Hanford <mark@hanfordonline.co.uk>
2012-01-17 15:28:48 +0000

    Added example sketch

--- examples/SerialGLCDexample/SerialGLCDexample.ino
+++ examples/SerialGLCDexample/SerialGLCDexample.ino
@@ -1,21 +1,31 @@
+// SerialGLCD requires SoftwareSerial to work, so make sure that's included too
+#include <SerialGLCD.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(2, 3);
+// Create a new SerialGLCD object, and specify:
+//   the transmit pin it is connected to (must be a PWM-capable pin)
+//   the width of the screen in pixels
+//   the height of the screen in pixels
+SerialGLCD glcd(3, 128, 64);
 
-void setup()  
-{
-  Serial.begin(57600);
-  Serial.println("Goodnight moon!");
+// Example setup
+void setup() {
 
-  // set the data rate for the SoftwareSerial port
-  mySerial.begin(4800);
-  mySerial.println("Hello, world?");
+  // initialise the Serial connection and clear the screen
+  glcd.begin();
+  
+  // draw some little box in the middle of the screen
+  glcd.drawBox(59, 27, 79, 47);
+  delay(500);
+  glcd.drawBox(49, 22, 74, 42);
+  delay(500);
+  glcd.drawBox(54, 17, 69, 37);
+  delay(2000);
+
+  // put some plain text
+  glcd.drawAscii("Hello!");
 }
 
-void loop() // run over and over
-{
-  if (mySerial.available())
-    Serial.write(mySerial.read());
-  if (Serial.available())
-    mySerial.write(Serial.read());
+// Example loop
+void loop() {
 }
